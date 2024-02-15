0f526c900a758e45b77c948d660c8471edd3e804
high-haseeb <haseebkhalidoriginal@gmail.com>
2024-02-15 05:52:40 +0500

    added shaders

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import Blob from "@/components/Blob";
-
+import Scene from "@/components/Blob"
 export default function Home() {
   return (
-    <div className="w-screen h-screen">
-      <Blob/>
+    <div className="w-screen h-screen overflow-hidden">
+      <Scene/>
     </div>
   );
 }
--- components/Blob.jsx
+++ components/Blob.jsx
@@ -1,44 +1,58 @@
 "use client";
-import {
-  Environment,
-  MeshWobbleMaterial,
-  PresentationControls,
-  Stars,
-} from "@react-three/drei";
+import { Environment, MeshWobbleMaterial, PresentationControls, Stars, Sphere } from "@react-three/drei";
 import { Canvas, useFrame } from "@react-three/fiber";
-import React, { Suspense, useEffect, useRef } from "react";
+import React, { Suspense, useMemo, useEffect, useRef } from "react";
+import fragmentShader from "@/shaders/fragmentShader.glsl";
+import vertexShader from "@/shaders/vertexShader.glsl";
+import { MathUtils } from "three";
 
-const Blob = () => {
+const Scene = () => {
   return (
-    <Suspense fallback={
-      <div className="w-screen h-screen bg-black flex items-center justify-center text-white text-9xl">Loading..</div>
-    }>
-    <Canvas className="bg-black">
-      <Stars />
-      <Environment preset="dawn" />
-      <PresentationControls>
-        <Model />
-      </PresentationControls>
-    </Canvas></Suspense>
+    <Suspense fallback={<div className="w-screen h-screen bg-black flex items-center justify-center text-white text-9xl">Loading..</div>}>
+      <Canvas className="bg-black">
+        <Environment preset="dawn" />
+        <ambientLight />
+        <PresentationControls>
+          <Blob />
+        </PresentationControls>
+      </Canvas>
+    </Suspense>
   );
 };
 
-export default Blob;
-const Model = () => {
-  const ref = useRef();
-  useFrame(() => {
-    if (ref.current) {
-      ref.current.rotation.y += 0.01;
-    }
+export default Scene;
+const Blob = () => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef();
+  const hover = useRef(false);
+
+  const uniforms = useMemo(
+    () => ({
+      u_intensity: {
+        value: 0.3,
+      },
+      u_time: {
+        value: 0.0,
+      },
+    }),
+    [],
+  );
+
+  useFrame((state) => {
+    const { clock } = state;
+    mesh.current.material.uniforms.u_time.value = 0.4 * clock.getElapsedTime();
+
+    mesh.current.material.uniforms.u_intensity.value = MathUtils.lerp(
+      mesh.current.material.uniforms.u_intensity.value,
+      hover.current ? 0.85 : 0.15,
+      0.02,
+    );
   });
+
   return (
-    <mesh
-      scale={2}
-      rotation={[0, 0, Math.PI / 3]}
-      ref={(el) => (ref.current = el)}
-    >
-      <sphereGeometry />
-      <MeshWobbleMaterial factor={30} speed={1} color={"pink"} />
+    <mesh ref={mesh} position={[0, 0, 0]} scale={1} onPointerOver={() => (hover.current = true)} onPointerOut={() => (hover.current = false)}>
+      <icosahedronGeometry args={[2, 20]} />
+      <shaderMaterial fragmentShader={fragmentShader} vertexShader={vertexShader} uniforms={uniforms} wireframe={false} />
     </mesh>
   );
 };
